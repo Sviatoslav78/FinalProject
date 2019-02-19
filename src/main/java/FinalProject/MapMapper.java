f0661a6da7c03b71d67311c054cf290aef635935
kseniya.tarantsova@gmail.com <developertarantsova>
2019-02-19 14:13:41 +0200

    added comment for NumberMapper and commented

--- src/main/java/FinalProject/MapMapper.java
+++ src/main/java/FinalProject/MapMapper.java
@@ -6,12 +6,6 @@ import java.util.*;
 public class MapMapper implements JsonMapper<Map> {
     @Override
     public void write(Map obj, MyJsonWriterStream writer) {
-        Collection key = obj.keySet();
-        Collection value = obj.values();
-        Collection s = new ArrayList();
-        for (Object o : s) {
-
-        }
 
     }
 }
--- src/main/java/FinalProject/NumberMapper.java
+++ src/main/java/FinalProject/NumberMapper.java
@@ -2,9 +2,10 @@ package FinalProject;
 
 public class NumberMapper implements JsonMapper <Number> {
 
-
+    //creates record of Number.class objects
     @Override
     public void write(Number obj, MyJsonWriterStream writer) {
-
+        writer.writeNumber(obj);
+        writer.flush();
     }
 }
