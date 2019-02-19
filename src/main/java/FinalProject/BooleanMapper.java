737f35086d4fc1fbbfd39f961a8b16896ed8b232
kseniya.tarantsova@gmail.com <developertarantsova>
2019-02-19 14:05:42 +0200

    added comment for BooleanMapper

--- src/main/java/FinalProject/BooleanMapper.java
+++ src/main/java/FinalProject/BooleanMapper.java
@@ -2,6 +2,7 @@ package FinalProject;
 
 public class BooleanMapper implements JsonMapper <Boolean>{
 
+    //creates record of Boolean.class objects
     @Override
     public void write(Boolean obj, MyJsonWriterStream writer) {
         writer.writeBoolean (obj);
