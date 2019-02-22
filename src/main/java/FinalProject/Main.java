1cfef1502e0f1233b7c1644040e828da925527f9
kseniya.tarantsova@gmail.com <developertarantsova>
2019-02-22 22:19:43 +0200

    was deleted custom class cat

--- src/main/java/FinalProject/Main.java
+++ src/main/java/FinalProject/Main.java
@@ -5,24 +5,22 @@ import com.google.gson.Gson;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.reflect.Field;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        ArrayList arrayList = new ArrayList();
-        arrayList.add("YO");
-        arrayList.add('m');
-        arrayList.add(53);
+        Map<String,Integer> map=new HashMap<>();
+        map.put("Monday",7);
+        map.put("Tuesday",1);
+        map.put("Wednesday",2);
 
-        Object[] map = new Object[]{"Hello","Mama", arrayList};
+        Runnable task = () -> {
+            System.out.println(new MyJsonSerializer().serialize(map));
+        };
 
-        MyJsonSerializer myJsonSerializer = new MyJsonSerializer();
-        System.out.println(myJsonSerializer.serialize(map));
-
-        Gson gson = new Gson();
-        System.out.println("GSON" + gson.toJson(map));
-
+        for (int i = 0; i < 3; i++) {
+            new Thread(task).start();
+        }
     }
 }
